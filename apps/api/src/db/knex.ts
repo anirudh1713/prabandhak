06b566d145aa6c2b93d16086a96eb762b1f7d6c8
anirudh1713 <anirudhatodaria11@gmail.com>
2022-08-14 13:49:12 +0530

    feat: add knex snake case mapper

--- apps/api/src/db/knex.ts
+++ apps/api/src/db/knex.ts
@@ -1,7 +1,11 @@
+import {knexSnakeCaseMappers} from 'objection';
 import {knex} from 'knex';
 import knexConfig from '../knexfile';
 
 // TODO - differentiate dev, prod and test configs.
-const knexInstance = knex(knexConfig);
+const knexInstance = knex({
+  ...knexConfig,
+  ...knexSnakeCaseMappers(),
+});
 
 export default knexInstance;
