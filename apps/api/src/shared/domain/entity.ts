d5c1e993b0c1d9314e0fa0cb0a26c07f8c0f2148
anirudh1713 <anirudhatodaria11@gmail.com>
2022-10-05 16:27:51 +0530

    feat: add base entity and value object classes (#16)

---
+++ apps/api/src/shared/domain/entity.ts
@@ -0,0 +1,32 @@
+import {EntityID} from './entity-id';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isEntity = (v: any): v is Entity<any> => {
+  return v instanceof Entity;
+};
+
+export abstract class Entity<T> {
+  protected readonly _id: EntityID;
+  public readonly props: T;
+
+  protected constructor(properties: T, id?: EntityID) {
+    this._id = id || new EntityID();
+    this.props = properties;
+  }
+
+  public equals(object?: Entity<T>): boolean {
+    if (!object) {
+      return false;
+    }
+
+    if (this === object) {
+      return true;
+    }
+
+    if (!isEntity(object)) {
+      return false;
+    }
+
+    return this._id.equals(object._id);
+  }
+}
