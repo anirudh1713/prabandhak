e47ec8bde4eb551abc546beb1043c55769fb6d95
anirudh1713 <anirudhatodaria11@gmail.com>
2022-10-17 16:42:36 +0530

    refactor: add user interface and response payloads
    - add `User` interface in gql schema
    - add type `Viewer` that implements `User` for current user
    - rename `createUser` mutation to `registerUser`
    - rename `user` query to `me` (soon, use current user from auth when its implemented)
    - add input type and payload for `registerUser` mutation

--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -1,4 +1,8 @@
-import {GQLUser} from '../../generated/graphql.generated';
+import {
+  GQLRegisterUserInput,
+  GQLRegisterUserPayload,
+  GQLViewer,
+} from '../../generated/graphql.generated';
 import {User} from './domain/user.entity';
 import {UserFirstName} from './domain/user-first-name';
 import {UserPassword} from './domain/user-password';
@@ -6,16 +10,15 @@ import {UserLastName} from './domain/user-last-name';
 import {UserEmail} from './domain/user-email';
 import {UserMapper} from './mappers/user.mapper';
 import {userRepo} from './repositories';
-import {IUserDTO} from './dtos/user.dto';
 
-export const getUserById = async (id: GQLUser['id']): Promise<IUserDTO> => {
+export const getUserById = async (id: GQLViewer['id']): Promise<GQLViewer> => {
   const user = await userRepo.getUserById(id);
   return UserMapper.toDTO(user);
 };
 
 export const createUser = async (
-  user: Omit<GQLUser, 'id'>,
-): Promise<IUserDTO> => {
+  user: GQLRegisterUserInput,
+): Promise<GQLRegisterUserPayload> => {
   const userData = User.create({
     lastName: UserLastName.create(user.lastName),
     firstName: UserFirstName.create(user.firstName),
