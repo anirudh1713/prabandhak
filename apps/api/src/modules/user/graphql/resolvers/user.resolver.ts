61ebe36c9f2e7ff86877e80fa4e11a1f20758ca5
anirudh1713 <anirudhatodaria11@gmail.com>
2022-12-22 21:03:49 +0530

    feat: implement `Node` interface to support relay specs (#24)

--- apps/api/src/modules/user/graphql/resolvers/user.resolver.ts
+++ apps/api/src/modules/user/graphql/resolvers/user.resolver.ts
@@ -3,8 +3,11 @@ import * as UserService from '../../user.service';
 import {NotFoundError} from '../../../../lib/custom-errors/not-found';
 import {UserMapper} from '../../mappers/user.mapper';
 import {UserID} from '../../domain/user-id';
+import {GQLResolvers} from '../../../../generated/graphql';
+import {globalObjectIDService} from '../../../../lib/global-object-id/global-object-id.service';
+import {TEntityType} from '../../../../shared/domain/types';
 
-const resolvers: UserModule.Resolvers = {
+const resolvers: UserModule.Resolvers & {Node: GQLResolvers['Node']} = {
   Query: {
     me: async (_, arguments_) => {
       const userEntityID = UserID.create(arguments_.id);
@@ -16,12 +19,32 @@ const resolvers: UserModule.Resolvers = {
 
       return UserMapper.toDTO(user);
     },
+    node: async (_, arguments_) => {
+      const [type] = <[TEntityType, string]>(
+        globalObjectIDService.parse(arguments_.id)
+      );
+
+      if (type !== 'Viewer') {
+        throw new Error(`type ${type} is invalid`);
+      }
+
+      const userEntityID = UserID.create(arguments_.id);
+      const user = await UserService.getUserById(userEntityID);
+
+      return UserMapper.toDTO(user);
+    },
   },
   Mutation: {
     registerUser: async (_, {registerUserInput}) => {
       const user = await UserService.createUser(registerUserInput, 'Viewer');
 
       return UserMapper.toDTO(user);
+    },
+  },
+  Node: {
+    __resolveType: ({id}) => {
+      const [type] = globalObjectIDService.parse(id);
+      return type as TEntityType;
     },
   },
 };
